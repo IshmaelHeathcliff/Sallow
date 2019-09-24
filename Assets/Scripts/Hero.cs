3ed998f7ea24db34e14162c041fd980bc53d0e9d
Ishmael Heathcliff <darkflare1055@gmail.com>
2019-09-24 16:15:58 +0800

    init repository

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private static Hero _instance;
+
+    public static Hero Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = gameObject.AddComponent<Hero>();
+            }
+
+            return _instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Move()
+    {
+        throw new NotImplementedException();
+    }
+
+    void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
