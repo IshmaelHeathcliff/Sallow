d56ae983da10d4f91d000d463b3e083b1520fa07
Ishmael Heathcliff <darkflare1055@gmail.com>
2019-09-24 21:35:24 +0800

    add Player Move

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,10 +9,7 @@ public class PlayerInput : MonoBehaviour
 {
     private static PlayerInput _instance;
     public static PlayerInput Instance { get; private set; }
-    public InputButton leftArrow = new InputButton(KeyCode.LeftArrow);
-    public InputButton rightArrow = new InputButton(KeyCode.RightArrow);
-    public InputButton downArrow = new InputButton(KeyCode.DownArrow);
-    public InputButton upArrow = new InputButton(KeyCode.UpArrow);
+    public InputAxis moveController = new InputAxis();
     public InputButton pause = new InputButton(KeyCode.Escape);
     public InputButton confirm = new InputButton(KeyCode.Return);
     public InputButton attack = new InputButton(KeyCode.A);
@@ -23,5 +20,13 @@ public class PlayerInput : MonoBehaviour
         {
             Instance = this;
         }
+    }
+
+    private void Update()
+    {
+        moveController.Get();
+        pause.Get();
+        confirm.Get();
+        attack.Get();
     }
 }
